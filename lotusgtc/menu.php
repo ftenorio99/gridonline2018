c73933c8d979058b35a72d7e48b078ab28ab3197
gridonline <f.tenorio.email@gmail.com>
2018-04-16 15:15:36 -0300

    att

--- lotusgtc/menu.php
+++ lotusgtc/menu.php
@@ -125,7 +125,7 @@ foreach($result as $row)
                 <!-- <li><a href='pilotosclassificados.php'>Pilotos Classificados para Próxima etapa</a></li> -->
                 <li><a href="classificacaov2.php">Campeonato de Pilotos</a></li>
                 <li><a href="classificacaoequipev2.php">Campeonato de Equipes</a></li>
-                <li><a href="lotusgtcresultados.php">Calendário/Resutados</a></li>
+                <li><a href="lotusgtcresultados.php">Calendário/Resultados</a></li>
                 <li><a href="download.php">Downloads</a></li>
                 <li><a href="regulamento.php">Regulamento</a></li>
                 <li><a href="pagamento.php">Pagamento de Inscrição</a></li>
